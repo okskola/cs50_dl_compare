2aef6df44ddb18a56c556b48186e831617234837
okskola <53981730+okskola@users.noreply.github.com>
2023-03-27 20:30:25 +0000

    Download moved to folder "files"

--- comp50.sh
+++ comp50.sh
@@ -4,7 +4,8 @@
 
 
 echo "Starting compare"
-compare50 * -x "dl.csv" -x "dl.py" -x "comp50.sh" 
+#compare50 * -x "dl.csv" -x "dl.py" -x "comp50.sh" 
+compare50 "files/*"
 echo "Done compare"
 zip -r results.zip results/
 echo "Done zipping. Please download results.zip"
